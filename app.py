b2359db36f534674d58fb8f1c98fea34f2e63ad1
katerynaoberemok <56259269+katerynaoberemok@users.noreply.github.com>
2019-10-08 15:59:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
-# import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
+
+
 # import matplotlib
 # import datetime as dt
 # import datetime
